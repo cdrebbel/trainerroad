097a1a70c35a4d9d41df8607faca1bc94ecf7d73
cdrebbel <cdrebbel@gmail.com>
2015-08-13 19:35:02 +0000

    Switching to NUnit

--- BikeDistributor.Test/OrderTest.cs
+++ BikeDistributor.Test/OrderTest.cs
@@ -1,15 +1,14 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+﻿using NUnit.Framework;
 namespace BikeDistributor.Test
 {
-    [TestClass]
+    [TestFixture]
     public class OrderTest
     {
         private readonly static Bike Defy = new Bike("Giant", "Defy 1", Bike.OneThousand);
         private readonly static Bike Elite = new Bike("Specialized", "Venge Elite", Bike.TwoThousand);
         private readonly static Bike DuraAce = new Bike("Specialized", "S-Works Venge Dura-Ace", Bike.FiveThousand);
 
-        [TestMethod]
+        [Test]
         public void ReceiptOneDefy()
         {
             var order = new Order("Anywhere Bike Shop");
@@ -23,7 +22,7 @@ Sub-Total: $1,000.00
 Tax: $72.50
 Total: $1,072.50";
 
-        [TestMethod]
+        [Test]
         public void ReceiptOneElite()
         {
             var order = new Order("Anywhere Bike Shop");
@@ -37,7 +36,7 @@ Sub-Total: $2,000.00
 Tax: $145.00
 Total: $2,145.00";
 
-        [TestMethod]
+        [Test]
         public void ReceiptOneDuraAce()
         {
             var order = new Order("Anywhere Bike Shop");
@@ -51,7 +50,7 @@ Sub-Total: $5,000.00
 Tax: $362.50
 Total: $5,362.50";
 
-        [TestMethod]
+        [Test]
         public void HtmlReceiptOneDefy()
         {
             var order = new Order("Anywhere Bike Shop");
@@ -61,7 +60,7 @@ Total: $5,362.50";
 
         private const string HtmlResultStatementOneDefy = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Giant Defy 1 = $1,000.00</li></ul><h3>Sub-Total: $1,000.00</h3><h3>Tax: $72.50</h3><h2>Total: $1,072.50</h2></body></html>";
 
-        [TestMethod]
+        [Test]
         public void HtmlReceiptOneElite()
         {
             var order = new Order("Anywhere Bike Shop");
@@ -71,7 +70,7 @@ Total: $5,362.50";
 
         private const string HtmlResultStatementOneElite = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Specialized Venge Elite = $2,000.00</li></ul><h3>Sub-Total: $2,000.00</h3><h3>Tax: $145.00</h3><h2>Total: $2,145.00</h2></body></html>";
 
-        [TestMethod]
+        [Test]
         public void HtmlReceiptOneDuraAce()
         {
             var order = new Order("Anywhere Bike Shop");
