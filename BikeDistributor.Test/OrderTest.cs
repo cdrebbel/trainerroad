eb1dbfe30d86d000fd98e1ca298f975fd270250e
cdrebbel <cdrebbel@gmail.com>
2015-08-13 20:11:03 +0000

    Refactored HTMLReceipt tests

--- BikeDistributor.Test/OrderTest.cs
+++ BikeDistributor.Test/OrderTest.cs
@@ -13,7 +13,7 @@ namespace BikeDistributor.Test
         [TestCase("Specialized","S-Works Venge Dura-Ace",Bike.FiveThousand, 362.50, 5362.50)]
         [TestCase("Specialized","Venge Elite",Bike.TwoThousand,145.00,2145.00)]
         [TestCase("Giant","Defy 1",Bike.OneThousand, 72.50, 1072.5)]
-        public void ReceiptOneDefy(string make, string model, int cost, decimal tax, decimal total)
+        public void ReceiptIsValid(string make, string model, int cost, decimal tax, decimal total)
         {
             var bike = new Bike(make, model, cost);
             var order = new Order("Anywhere Bike Shop");
@@ -26,36 +26,19 @@ Total: {4:C}", make, model, cost,tax,total);
 
             order.Receipt().Should().Be(expected);
         }
-        [Test]
-        public void HtmlReceiptOneDefy()
+
+        [TestCase("Specialized", "S-Works Venge Dura-Ace", Bike.FiveThousand, 362.50, 5362.50)]
+        [TestCase("Specialized", "Venge Elite", Bike.TwoThousand, 145.00, 2145.00)]
+        [TestCase("Giant", "Defy 1", Bike.OneThousand, 72.50, 1072.5)]
+        public void HtmlReceiptIsValid(string make, string model, int cost, decimal tax, decimal total)
         {
+            var bike = new Bike(make, model, cost);
             var order = new Order("Anywhere Bike Shop");
-            order.AddLine(new Line(Defy, 1));
-            Assert.AreEqual(HtmlResultStatementOneDefy, order.HtmlReceipt());
-        }
-
-        private const string HtmlResultStatementOneDefy = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Giant Defy 1 = $1,000.00</li></ul><h3>Sub-Total: $1,000.00</h3><h3>Tax: $72.50</h3><h2>Total: $1,072.50</h2></body></html>";
-
-        [Test]
-        public void HtmlReceiptOneElite()
-        {
-            var order = new Order("Anywhere Bike Shop");
-            order.AddLine(new Line(Elite, 1));
-            Assert.AreEqual(HtmlResultStatementOneElite, order.HtmlReceipt());
-        }
-
-        private const string HtmlResultStatementOneElite = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Specialized Venge Elite = $2,000.00</li></ul><h3>Sub-Total: $2,000.00</h3><h3>Tax: $145.00</h3><h2>Total: $2,145.00</h2></body></html>";
-
-        [Test]
-        public void HtmlReceiptOneDuraAce()
-        {
-            var order = new Order("Anywhere Bike Shop");
-            order.AddLine(new Line(DuraAce, 1));
-            Assert.AreEqual(HtmlResultStatementOneDuraAce, order.HtmlReceipt());
-        }
-
-        private const string HtmlResultStatementOneDuraAce = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Specialized S-Works Venge Dura-Ace = $5,000.00</li></ul><h3>Sub-Total: $5,000.00</h3><h3>Tax: $362.50</h3><h2>Total: $5,362.50</h2></body></html>";    
-    }
+            order.AddLine(new Line(bike, 1));
+            var expected = string.Format(@"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x {0} {1} = {2:C}</li></ul><h3>Sub-Total: {2:C}</h3><h3>Tax: {3:C}</h3><h2>Total: {4:C}</h2></body></html>",
+                 make, model, cost, tax, total);
+            order.HtmlReceipt().Should().Be(expected);
+        }}
 }
 
 
